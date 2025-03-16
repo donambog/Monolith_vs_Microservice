8c280021cea1d0d05402170a9c62b85b02c338c1
Donald Mbogni <dmbogni@ymail.com>
2025-03-16 23:20:19 +0100

    reogranuse directories to be consistent with the notes in readme

---
+++ user_service/user_service.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+users = [{"id": 1, "name": "Alice"}, {"id": 2, "name": "Bob"}]
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    return jsonify(users)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
